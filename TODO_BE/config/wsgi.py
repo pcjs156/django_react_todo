a271954eb010419f2fa994a672f8511eee2b01b5
Yubin Park <itqkrdbqls1001@naver.com>
2021-09-04 15:37:33 +0900

    refactor: change project name from TODO_BE to config

--- TODO_BE/config/wsgi.py
+++ TODO_BE/config/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for TODO_BE project.
+WSGI config for config project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'TODO_BE.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 
 application = get_wsgi_application()

9123e88c448f873c1816d2e5d691114e932cc0d9
Yubin Park <itqkrdbqls1001@naver.com>
2021-09-05 14:42:05 +0900

    feat: Add test data to FE index.html

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,7 +3,6 @@ import './App.css';
 import TodoContainer from './TodoContainer';
 
 function App() {
-  console.log(window.itemObjects);
   return (
     <div>
       <TodoContainer boardName={window.name} todoList={window.todoList} />
